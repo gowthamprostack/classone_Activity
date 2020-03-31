b9c9467691df8c01ea606c35e0cbbee97d5e013d
saawq <carrygowtham.gmail.com>
2020-03-31 08:53:36 +0530

    Initial commitone

---
+++ app/src/main/java/com/example/classone_activity/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.classone_activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button button1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button1=(Button)findViewById(R.id.button1);
+    }
+
+    public void onClick(View v) {
+        Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+        startActivity(intent);
+    }
+}
